839e3f150a3ded6eff6707447229c113077a1862
frozonfreak <shazcool@gmail.com>
2015-01-09 13:38:48 +0800

    update table name

--- server/session/db_session.php
+++ server/session/db_session.php
@@ -16,7 +16,7 @@ class DB_Session{
 	public function checkIfSessionExist($session_id){
 		try{
 			$db = new PDO(DB_STRING, DB_USER, DB_PASSWORD);
-			$sql = "SELECT COUNT(session_id) FROM cacani_session WHERE session_id = :session_id";
+			$sql = "SELECT COUNT(session_id) FROM user_session WHERE session_id = :session_id";
 			$stmt = $db->prepare($sql);
 			$stmt->execute(array(':session_id' => $session_id));
 			$response = $stmt->fetch();
@@ -33,7 +33,7 @@ class DB_Session{
 	public function getSessionTime($session_id){
 		try{
 			$db = new PDO(DB_STRING, DB_USER, DB_PASSWORD);
-			$sql = "SELECT session_time FROM cacani_session WHERE session_id = :session_id";
+			$sql = "SELECT session_time FROM user_session WHERE session_id = :session_id";
 			$stmt = $db->prepare($sql);
 			$stmt->execute(array(':session_id' => $session_id));
 			$response = $stmt->fetch();
@@ -50,7 +50,7 @@ class DB_Session{
 	public function createSessionInDB($userName, $remote_addr, $remote_host, $remote_port, $remote_user, $user_agent, $session_id){
 		try{
 			$db = new PDO(DB_STRING, DB_USER, DB_PASSWORD);
-			$sql = "INSERT INTO cacani_session(session_id, user_name, remote_host, remote_port, remote_user, remote_addr, user_agent, session_date, session_time) values (:session_id, :user_name, :remote_host, :remote_port, :remote_user, :remote_addr, :user_agent, NOW(), :session_time)";
+			$sql = "INSERT INTO user_session(session_id, user_name, remote_host, remote_port, remote_user, remote_addr, user_agent, session_date, session_time) values (:session_id, :user_name, :remote_host, :remote_port, :remote_user, :remote_addr, :user_agent, NOW(), :session_time)";
 			$response = $db->prepare($sql);
 			$response->execute(array(':session_id' => $session_id, ':user_name' => $userName, ':remote_host' => $remote_host, ':remote_port' => $remote_port, ':remote_user' => $remote_user, ':remote_addr' => $remote_addr, ':user_agent'=> $user_agent, ':session_time' => time()));
 			$db = null;
